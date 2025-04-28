51af8034f7ee7a10fd032d575da3f078b82af271
omegafrog <jiwoo22110103@gmail.com>
2025-04-28 15:11:39 +0900

    feat: RedisConfig 추가

--- service/queue-server/src/main/java/org/codeNbug/queueserver/external/redis/RedisConfig.java
+++ service/queue-server/src/main/java/org/codeNbug/queueserver/external/redis/RedisConfig.java
@@ -4,20 +4,40 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-	private static final String WAITING_QUEUE_GROUP_NAME = "WAITING_QUEUE";
-	private static final String WAITING_QUEUE_KEY_NAME = "WAITING";
+	// 컨슈머 생성 시 그룹 이름
+	public static final String WAITING_QUEUE_GROUP_NAME = "WAITING_QUEUE";
+	// redis stream 키 이름
+	public static final String WAITING_QUEUE_KEY_NAME = "WAITING";
+	// 메시지의 idx 값을 저장하기 위한 space의 key값
+	public static final String WAITING_QUEUE_IDX_KEY_NAME = "WAITING_QUEUE_IDX";
+	// 메시지 내부의 userId 속성의 키 값
+	public static final String WAITING_QUEUE_MESSAGE_USER_ID_KEY_NAME = "userId";
+	// 메시지 내부의 eventId 속성의 키 값
+	public static final String WAITING_QUEUE_MESSAGE_EVENT_ID_KEY_NAME = "eventId";
+	// 메시지 내부의 idx 속성의 키 값
+	public static final String WAITING_QUEUE_MESSAGE_IDX_KEY_NAME = "idx";
+
 
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
+		StringRedisSerializer keySerializer = new StringRedisSerializer();
+		// 메시지 필드와 값은 JSON
+		GenericJackson2JsonRedisSerializer jsonSerializer =
+			new GenericJackson2JsonRedisSerializer();
 		redisTemplate.setConnectionFactory(redisConnectionFactory);
-		redisTemplate.opsForStream()
-			.createGroup(WAITING_QUEUE_KEY_NAME, WAITING_QUEUE_GROUP_NAME);
+		redisTemplate.setKeySerializer(keySerializer);
+		redisTemplate.setHashKeySerializer(keySerializer);
+		redisTemplate.setHashValueSerializer(jsonSerializer);
+		redisTemplate.setValueSerializer(jsonSerializer);
+
 		return redisTemplate;
 	}
+
 }
