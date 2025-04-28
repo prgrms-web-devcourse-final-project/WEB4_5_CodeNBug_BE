a8083bd73bb7c693ad7ff2bfbafd1e1d6fb8fa39
omegafrog <jiwoo22110103@gmail.com>
2025-04-28 17:36:55 +0900

    feat: 스케줄링 활성화

--- service/queue-server/src/main/java/org/codeNbug/queueserver/global/GlobalConfig.java
+++ service/queue-server/src/main/java/org/codeNbug/queueserver/global/GlobalConfig.java
@@ -4,8 +4,10 @@ import org.codeNbug.mainserver.user.entity.User;
 import org.codeNbug.mainserver.user.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 public class GlobalConfig {
 	@Bean
 	public UserService mockUserService() {
