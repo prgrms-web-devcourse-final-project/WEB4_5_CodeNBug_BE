14d0860657f75ab274e2f83e62b238fe07ebc226
omegafrog <jiwoo22110103@gmail.com>
2025-04-28 11:03:35 +0900

    feat: SseEmitterService.add 작성
    새로운 emitter를 작성하고 저장

---
+++ service/queue-server/src/main/java/org/codeNbug/queueserver/waitingqueue/service/SseEmitterService.java
@@ -0,0 +1,31 @@
+package org.codeNbug.queueserver.waitingqueue.service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+public class SseEmitterService {
+
+	private static Map<Long, SseEmitter> emitterMap = new ConcurrentHashMap<Long, SseEmitter>();
+
+	public void add(Long userId, Long eventId) {
+		// 새로운 emitter 생성
+		SseEmitter emitter = new SseEmitter();
+		emitter.onCompletion(() -> emitterMap.remove(userId));
+
+		// 초기 메시지 전달
+		try {
+			emitter.send(
+				SseEmitter.event()
+					.data("sse 연결 성공. userId:" + userId));
+		} catch (Exception e) {
+			emitter.complete();
+		}
+
+		// 전역 공간에 emitter 저장
+		emitterMap.put(userId, emitter);
+	}
+}
