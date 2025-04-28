0e1d6b7880e4e0a33cca0847c3c6094b723aec53
omegafrog <jiwoo22110103@gmail.com>
2025-04-28 17:35:55 +0900

    feat: redis stream을 사용하기 전에 사전 설정 추가

--- service/queue-server/src/main/java/org/codeNbug/queueserver/external/redis/RedisConfig.java
+++ service/queue-server/src/main/java/org/codeNbug/queueserver/external/redis/RedisConfig.java
@@ -17,26 +17,38 @@ public class RedisConfig {
 	// 메시지의 idx 값을 저장하기 위한 space의 key값
 	public static final String WAITING_QUEUE_IDX_KEY_NAME = "WAITING_QUEUE_IDX";
 	// 메시지 내부의 userId 속성의 키 값
-	public static final String WAITING_QUEUE_MESSAGE_USER_ID_KEY_NAME = "userId";
+	public static final String WAITING_QUEUE_MESSAGE_USER_ID_KEY_NAME = "usrId";
 	// 메시지 내부의 eventId 속성의 키 값
 	public static final String WAITING_QUEUE_MESSAGE_EVENT_ID_KEY_NAME = "eventId";
 	// 메시지 내부의 idx 속성의 키 값
 	public static final String WAITING_QUEUE_MESSAGE_IDX_KEY_NAME = "idx";
 
-
 	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+	public RedisTemplate<String, Object> basicRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		StringRedisSerializer keySerializer = new StringRedisSerializer();
 		// 메시지 필드와 값은 JSON
 		GenericJackson2JsonRedisSerializer jsonSerializer =
 			new GenericJackson2JsonRedisSerializer();
+
 		redisTemplate.setConnectionFactory(redisConnectionFactory);
 		redisTemplate.setKeySerializer(keySerializer);
 		redisTemplate.setHashKeySerializer(keySerializer);
 		redisTemplate.setHashValueSerializer(jsonSerializer);
 		redisTemplate.setValueSerializer(jsonSerializer);
+		redisTemplate.afterPropertiesSet();
 
+		// 컨슈머 그룹이 없으면 새로운 컨슈머 그룹 생성
+		if (redisTemplate.opsForStream()
+			.groups(WAITING_QUEUE_KEY_NAME).isEmpty()) {
+			redisTemplate.opsForStream()
+				.createGroup(WAITING_QUEUE_KEY_NAME, WAITING_QUEUE_GROUP_NAME);
+		}
+		// 메시지 idx 데이터가 없다면 생성.
+		if (redisTemplate.opsForValue().get(WAITING_QUEUE_IDX_KEY_NAME) == null) {
+			redisTemplate.opsForValue()
+				.set(WAITING_QUEUE_IDX_KEY_NAME, 0L);
+		}
 		return redisTemplate;
 	}
 
