c5f99273fd6dea5903cd04b1ebb80ba622c40df1
choijaewoo <cchoi00332244@gmail.com>
2025-04-28 11:43:58 +0900

    application-dev.yml resolved conflict

--- service/main-server/src/main/java/org/codeNbug/mainserver/MainServerApplication.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/MainServerApplication.java
@@ -2,8 +2,10 @@ package org.codeNbug.mainserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MainServerApplication {
 
 	public static void main(String[] args) {
