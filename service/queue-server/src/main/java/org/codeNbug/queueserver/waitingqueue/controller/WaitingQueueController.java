0727830051e76569e4dd6c539373f0149e79255a
omegafrog <jiwoo22110103@gmail.com>
2025-04-28 10:40:18 +0900

    feat: 대기열 컨트롤러 생성

---
+++ service/queue-server/src/main/java/org/codeNbug/queueserver/waitingqueue/controller/WaitingQueueController.java
@@ -0,0 +1,30 @@
+package org.codeNbug.queueserver.waitingqueue.controller;
+
+import org.codeNbug.queueserver.waitingqueue.service.WaitingQueueService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequestMapping("/api/v1")
+public class WaitingQueueController {
+
+	private final WaitingQueueService waitingQueueService;
+
+	public WaitingQueueController(WaitingQueueService waitingQueueService) {
+		this.waitingQueueService = waitingQueueService;
+	}
+
+	/**
+	 * 로그인한 유저가 행사 id가 {@code event-id}인 행사의 티켓 대기열에 진입합니다.
+	 * 대기열에 진입시 sse 연결을 진행합니다.
+	 * @param eventId
+	 */
+	@PostMapping("/events/{event-id}/tickets/waiting")
+	public void entryWaiting(@PathVariable("event-id") Long eventId) {
+		waitingQueueService.entry();
+	}
+}
