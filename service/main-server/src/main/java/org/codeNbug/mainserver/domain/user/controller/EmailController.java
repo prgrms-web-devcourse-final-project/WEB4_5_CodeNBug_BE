682883549c9f415b83e7174c8fbe09b075f76b05
joungHyeon <jounghyeon123@daum.net>
2025-04-29 16:32:55 +0900

    Feat: 응답 구조 변경

--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/user/controller/EmailController.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/user/controller/EmailController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.codeNbug.mainserver.domain.user.dto.EmailDto;
 import org.codeNbug.mainserver.domain.user.service.EmailService;
+import org.codeNbug.mainserver.global.dto.RsData;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,20 +22,30 @@ public class EmailController {
 
     // 인증코드 메일 발송
     @PostMapping("/send")
-    public ResponseEntity<String> mailSend(@RequestBody EmailDto.SendRequest request) throws MessagingException {
+    public ResponseEntity<RsData<Void>> mailSend(@RequestBody EmailDto.SendRequest request) {
         log.info("EmailController.mailSend()");
-        emailService.sendEmail(request.getMail());
-        return ResponseEntity.ok("인증코드가 발송되었습니다.");
+        try {
+            emailService.sendEmail(request.getMail());
+            return ResponseEntity.ok(
+                    new RsData<>("200-SUCCESS", "인증코드가 발송되었습니다."));
+        } catch (MessagingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new RsData<>("500-INTERNAL_SERVER_ERROR", "이메일 발송에 실패했습니다."));
+        }
     }
 
     // 인증코드 인증
     @PostMapping("/verify")
-    public ResponseEntity<String> verify(@RequestBody EmailDto.VerifyRequest request) {
+    public ResponseEntity<RsData<Void>> verify(@RequestBody EmailDto.VerifyRequest request) {
         log.info("EmailController.verify()");
         boolean isVerify = emailService.verifyEmailCode(request.getMail(), request.getVerifyCode());
+        
         if (!isVerify) {
-            return ResponseEntity.badRequest().body("인증에 실패했습니다.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new RsData<>("400-BAD_REQUEST", "인증에 실패했습니다."));
         }
-        return ResponseEntity.ok("인증이 완료되었습니다.");
+        
+        return ResponseEntity.ok(
+                new RsData<>("200-SUCCESS", "인증이 완료되었습니다."));
     }
 }
