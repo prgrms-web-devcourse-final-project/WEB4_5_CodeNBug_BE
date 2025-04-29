b716846097368eef296dea67dd980e0c34a8f74d
장선호 <92978663+ohnoesganj@users.noreply.github.com>
2025-04-29 12:38:08 +0900

    Feat/좌석 조회 (#43)
    * Fix: JavaDoc 반영
    
    * Feat: GlobalExceptionHandler 구현
    
    * Feat: GlobalExceptionHandler extends 삭제
    
    * Fix: Enum Class 어노테이션 삭제
    
    * Feat: handleIllegalArgumentException 추가
    
    * Feat: SeatLayoutResponse dto 추가
    
    * Feat: 좌석 조회 컨트롤러 로직 구현
    
    * Feat: 좌석 조회 레포지토리 로직 구현
    
    * Feat: 좌석 조회 서비스 로직 구현

--- service/main-server/src/main/java/org/codeNbug/mainserver/MainServerApplication.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/MainServerApplication.java
@@ -3,7 +3,6 @@ package org.codeNbug.mainserver;
 import org.codeNbug.mainserver.global.security.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/seat/controller/SeatController.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/seat/controller/SeatController.java
@@ -1,12 +1,13 @@
 package org.codeNbug.mainserver.domain.seat.controller;
 
 import org.codeNbug.mainserver.domain.seat.dto.SeatCancelRequest;
+import org.codeNbug.mainserver.domain.seat.dto.SeatLayoutResponse;
 import org.codeNbug.mainserver.domain.seat.dto.SeatSelectRequest;
 import org.codeNbug.mainserver.domain.seat.service.SeatService;
 import org.codeNbug.mainserver.global.dto.RsData;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,47 +23,56 @@ public class SeatController {
 	private SeatService seatService;
 
 	/**
+	 * 좌석 조회 API
+	 *
+	 * @param eventId 조회할 이벤트 ID
+	 * @return 좌석 선택 결과 응답
+	 */
+	@GetMapping("/{event-id}/seats")
+	public ResponseEntity<RsData> getSeatLayout(@PathVariable("event-id") Long eventId) {
+		SeatLayoutResponse seatLayoutResponse = seatService.getSeatLayout(eventId);
+		RsData response = new RsData(
+			"200",
+			"좌석 선택 성공",
+			seatLayoutResponse
+		);
+		return ResponseEntity.ok(response);
+	}
+
+	/**
 	 * 좌석 선택 API
 	 *
-	 * @param eventId           좌석이 포함된 이벤트 ID
+	 * @param eventId           조회할 이벤트 ID
 	 * @param seatSelectRequest 사용자가 선택한 좌석 ID 목록
 	 * @return 좌석 선택 결과 응답
 	 */
 	@PostMapping("/{event-id}/seats")
 	public ResponseEntity<RsData> selectSeat(@PathVariable("event-id") Long eventId,
 		@RequestBody SeatSelectRequest seatSelectRequest) {
-		try {
-			seatService.selectSeat(eventId, seatSelectRequest);
-			RsData response = new RsData(
-				"200",
-				"좌석 선택 성공",
-				seatSelectRequest
-			);
-			return ResponseEntity.ok(response);
-		} catch (IllegalStateException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body(new RsData("409", "이미 선택된 좌석입니다."));
-		}
+		seatService.selectSeat(eventId, seatSelectRequest);
+		RsData response = new RsData(
+			"200",
+			"좌석 선택 성공",
+			seatSelectRequest
+		);
+		return ResponseEntity.ok(response);
 	}
 
 	/**
 	 * 좌석 취소 API
 	 *
-	 * @param eventId           좌석이 포함된 이벤트 ID
+	 * @param eventId           조회할 이벤트 ID
 	 * @param seatCancelRequest 사용자가 취소한 좌석 ID 목록
 	 * @return 좌석 취소 결과 응답
 	 */
 	@DeleteMapping("/{event-id}/seats")
 	public ResponseEntity<RsData> CancelSeat(@PathVariable("event-id") Long eventId,
 		@RequestBody SeatCancelRequest seatCancelRequest) {
-		try {
-			seatService.cancelSeat(eventId, seatCancelRequest);
-			RsData response = new RsData(
-				"200",
-				"좌석 취소 성공"
-			);
-			return ResponseEntity.ok(response);
-		} catch (IllegalStateException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new RsData("404", "해당 좌석을 찾을 수 없습니다."));
-		}
+		seatService.cancelSeat(eventId, seatCancelRequest);
+		RsData response = new RsData(
+			"200",
+			"좌석 취소 성공"
+		);
+		return ResponseEntity.ok(response);
 	}
 }
