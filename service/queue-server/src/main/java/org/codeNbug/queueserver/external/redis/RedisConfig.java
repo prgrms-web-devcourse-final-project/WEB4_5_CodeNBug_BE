9a756835a8ece83d15f0cc1b1e9802df90cc71d1
omegafrog <jiwoo22110103@gmail.com>
2025-04-29 14:19:39 +0900

    Chore: 대기열 탈출
    Squashed commit:
    
    [7617600] Feat: entry queue의 클라이언트가 sse 연결을 해제했을 시에 다음에 가져올 최대 count 값을 올리도록 수정
    
    [6d340da] Feat: queue server에서 redis pub/sub으로 구독한 대기열 탈출 메시지를 받도록 구현
    
    pub/sub 메시지를 받고 대기열을 탈출했음을 유저에게 전달함.
    아직 연결은 유지되어 있음. ( 결제시 연결 해제 )
    
    [a66ecb2] Feat: entry queue의 메시지를 redis pub/sub으로 적재
    
    entry queue의 메시지는 어떤행사의 어떤 유저가 대기열을 빠져나왔는지를 저장한다.
    이 정보를 실제 클라이언트에게 전송하기 위해서
    각각의 sse 연결된 서버가 entry queue의 데이터를 구독하고 받도록 한다
    
    [86df61a] Feat: 서버의 ID를 entry queue 메시지에 추가
    
    [994464c] feat: waiting queue에서 메시지를 가져와 entry queue에 적재
    
    redis에서 ENTRY_QUEUE_COUNT를 보고 최대 몇개의 메시지를 가져올지 결정함.
    초기값은 1000개임.
    
    메시지를 가져와 entity queue에 적재
    
    이후에 다음번에 가져올 메시지 갯수를 계산하기 위해
    entry queue 최대 사이즈 - 이번에 가져온 메시지 갯수
    를 entry queue count로 저장함.
    
    [ffecbbd] feat: SseConnection 사용하도록 수정
    
    [71801a8] feat: SseConnection setter 추가
    
    [ffb9211] feat: entry queue에 있는 사용자와 연결이 끊어질 경우 dispatcher server로 알림
    
    [0a0b498] feat: redis 사용을 위한 설정 추가
    
    [b47596d] feat: 스레드 사용을 위한 설정 추가
    
    [68e6bfa] Init: messageDispatcher server 작성
    
    waiting queue -> entry queue로 메시지를 옮기고 옮겼음을 다른 서버에게 알림
    
    [bea78cf] Refactor: static 변수명 변경
    
    [7ed05c9] feat: 서버 인스턴스명을 포함하는 그룹명으로 변경
    
    [6f41a62] feat: 서버 인스턴스명 추가

--- service/queue-server/src/main/java/org/codeNbug/queueserver/external/redis/RedisConfig.java
+++ service/queue-server/src/main/java/org/codeNbug/queueserver/external/redis/RedisConfig.java
@@ -1,27 +1,52 @@
 package org.codeNbug.queueserver.external.redis;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-	// 컨슈머 생성 시 그룹 이름
+	// waiting queue 컨슈머 생성 시 그룹 이름
 	public static final String WAITING_QUEUE_GROUP_NAME = "WAITING_QUEUE";
-	// redis stream 키 이름
+	// waiting queue redis stream 키 이름
 	public static final String WAITING_QUEUE_KEY_NAME = "WAITING";
+	public static final Long ENTRY_QUEUE_CAPACITY = 1000L;
 	// 메시지의 idx 값을 저장하기 위한 space의 key값
 	public static final String WAITING_QUEUE_IDX_KEY_NAME = "WAITING_QUEUE_IDX";
+	// entry queue의 현재 인원을  저장하기 위한 space의 key값
+	public static final String ENTRY_QUEUE_COUNT_KEY_NAME = "ENTRY_QUEUE_COUNT";
 	// 메시지 내부의 userId 속성의 키 값
-	public static final String WAITING_QUEUE_MESSAGE_USER_ID_KEY_NAME = "userId";
+	public static final String QUEUE_MESSAGE_USER_ID_KEY_NAME = "userId";
 	// 메시지 내부의 eventId 속성의 키 값
-	public static final String WAITING_QUEUE_MESSAGE_EVENT_ID_KEY_NAME = "eventId";
+	public static final String QUEUE_MESSAGE_EVENT_ID_KEY_NAME = "eventId";
+	public static final String QUEUE_MESSAGE_INSTANCE_ID_KEY_NAME = "instanceId";
 	// 메시지 내부의 idx 속성의 키 값
-	public static final String WAITING_QUEUE_MESSAGE_IDX_KEY_NAME = "idx";
+	public static final String QUEUE_MESSAGE_IDX_KEY_NAME = "idx";
+	private static final String DISPATCH_QUEUE_CHANNEL_NAME = "DISPATCH";
+
+	@Value("${custom.instance-id}")
+	private String instanceId;
+
+	@Bean
+	@Qualifier("pubSubConnectionFactory")
+	public LettuceConnectionFactory redisPubSubConnectionFactory(
+		@Value("${spring.data.redis.host}") String host, @Value("${spring.data.redis.port}") int port
+	) {
+
+		LettuceConnectionFactory factory = new LettuceConnectionFactory(host, port);
+		factory.afterPropertiesSet();
+		return factory;
+	}
 
 	@Bean
 	public RedisTemplate<String, Object> basicRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -39,17 +64,48 @@ public class RedisConfig {
 		redisTemplate.afterPropertiesSet();
 
 		// 컨슈머 그룹이 없으면 새로운 컨슈머 그룹 생성
-		if (redisTemplate.opsForStream()
-			.groups(WAITING_QUEUE_KEY_NAME).isEmpty()) {
+		try {
+			if (!redisTemplate.opsForStream()
+				.groups(WAITING_QUEUE_KEY_NAME).stream().anyMatch(
+					xInfoGroup -> xInfoGroup.groupName().equals(WAITING_QUEUE_GROUP_NAME + ":" + instanceId)
+				)) {
+				redisTemplate.opsForStream()
+					.createGroup(WAITING_QUEUE_KEY_NAME, WAITING_QUEUE_GROUP_NAME + ":" + instanceId);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 			redisTemplate.opsForStream()
-				.createGroup(WAITING_QUEUE_KEY_NAME, WAITING_QUEUE_GROUP_NAME);
+				.createGroup(WAITING_QUEUE_KEY_NAME, WAITING_QUEUE_GROUP_NAME + ":" + instanceId);
 		}
 		// 메시지 idx 데이터가 없다면 생성.
 		if (redisTemplate.opsForValue().get(WAITING_QUEUE_IDX_KEY_NAME) == null) {
 			redisTemplate.opsForValue()
 				.set(WAITING_QUEUE_IDX_KEY_NAME, 0L);
 		}
+		// entry queue count 데이터가 없다면 생성
+		if (redisTemplate.opsForValue().get(ENTRY_QUEUE_COUNT_KEY_NAME) == null) {
+			redisTemplate.opsForValue()
+				.set(ENTRY_QUEUE_COUNT_KEY_NAME, ENTRY_QUEUE_CAPACITY);
+		}
 		return redisTemplate;
+	}
+
+	@Bean
+	public RedisMessageListenerContainer redisMessageListenerContainer(
+		@Qualifier("pubSubConnectionFactory") LettuceConnectionFactory cf,
+		MessageListenerAdapter listenerAdapter) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(cf);
+		// 구독할 채널을 등록
+		container.addMessageListener(listenerAdapter, new PatternTopic(RedisConfig.DISPATCH_QUEUE_CHANNEL_NAME));
+		// container.start();
+		return container;
+	}
+
+	@Bean
+	public MessageListenerAdapter listenerAdapter(Subscriber subscriber) {
+		// "onMessage" 메소드가 호출됩니다.
+		return new MessageListenerAdapter(subscriber, "onMessage");
 	}
 
 }
