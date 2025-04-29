59716dc5b481fcb474bc0e3b9c532cc492784fb7
ohnoesganj <sunho3456@naver.com>
2025-04-29 12:28:28 +0900

    Feat: 좌석 조회 서비스 로직 구현

--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/seat/service/SeatService.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/seat/service/SeatService.java
@@ -1,11 +1,15 @@
 package org.codeNbug.mainserver.domain.seat.service;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
 
 import org.codeNbug.mainserver.domain.seat.dto.SeatCancelRequest;
+import org.codeNbug.mainserver.domain.seat.dto.SeatLayoutResponse;
 import org.codeNbug.mainserver.domain.seat.dto.SeatSelectRequest;
 import org.codeNbug.mainserver.domain.seat.entity.Seat;
+import org.codeNbug.mainserver.domain.seat.entity.SeatLayout;
+import org.codeNbug.mainserver.domain.seat.repository.SeatLayoutRepository;
 import org.codeNbug.mainserver.domain.seat.repository.SeatRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,13 +25,29 @@ public class SeatService {
 
 	private final RedisLockService redisLockService;
 	private final SeatRepository seatRepository;
+	private final SeatLayoutRepository seatLayoutRepository;
 
 	private static final String SEAT_LOCK_KEY_PREFIX = "seat:lock:";
 
 	/**
-	 * 좌석 선택 요청에 따라 Redis 락을 걸고, DB에 좌석 예약을 반영
+	 * 주어진 이벤트 ID에 해당하는 좌석 목록 조회
 	 *
-	 * @param eventId           좌석이 포함된 이벤트 ID
+	 * @param eventId 조회할 이벤트 ID
+	 * @return SeatLayoutResponse 좌석 레이아웃
+	 * @throws IllegalArgumentException 존재하지 않는 이벤트 ID일 경우
+	 */
+	public SeatLayoutResponse getSeatLayout(Long eventId) {
+		SeatLayout seatLayout = seatLayoutRepository.findById(eventId)
+			.orElseThrow(() -> new IllegalArgumentException("행사가 존재하지 않습니다."));
+
+		List<Seat> seatList = seatRepository.findAllByLayoutIdWithGrade((seatLayout.getId()));
+		return new SeatLayoutResponse(seatList);
+	}
+
+	/**
+	 * 좌석 선택 요청에 따라 Redis 락을 걸고, DB에 좌석 상태 반영
+	 *
+	 * @param eventId           이벤트 ID
 	 * @param seatSelectRequest 선택한 좌석 ID 목록을 포함한 요청 객체
 	 * @throws IllegalStateException 이미 선택된 좌석이 있는 경우
 	 * @throws IllegalArgumentException 존재하지 않는 좌석이 포함된 경우
@@ -54,9 +74,9 @@ public class SeatService {
 	}
 
 	/**
-	 * 좌석 취소 요청에 따라 Redis 락을 해제하고, DB에 좌석 예약을 반영
+	 * 좌석 취소 요청에 따라 Redis 락을 해제하고, DB에 좌석 상태 반영
 	 *
-	 * @param eventId           좌석이 포함된 이벤트 ID
+	 * @param eventId           이벤트 ID
 	 * @param seatCancelRequest 선택한 좌석 ID 목록을 포함한 요청 객체
 	 * @throws IllegalArgumentException 존재하지 않는 좌석이 포함된 경우
 	 */
