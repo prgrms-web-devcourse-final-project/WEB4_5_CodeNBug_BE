5fa1c641af1ad3459ed31beb74016a334eca0438
choijaewoo <cchoi00332244@gmail.com>
2025-04-29 20:15:28 +0900

    feat: event soft delete 를 위한 isDeleted 필드 추가

---
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/manager/service/EventRemoveService.java
@@ -0,0 +1,10 @@
+package org.codeNbug.mainserver.domain.manager.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EventRemoveService {
+
+}
