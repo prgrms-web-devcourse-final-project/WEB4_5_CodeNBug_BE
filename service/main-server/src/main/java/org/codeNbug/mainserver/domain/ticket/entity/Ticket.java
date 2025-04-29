46b68005311474bd4a21fa473198a0f4e3715511
ohnoesganj <sunho3456@naver.com>
2025-04-29 12:53:14 +0900

    Fix: mock entity 삭제

--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/ticket/entity/Ticket.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/ticket/entity/Ticket.java
@@ -27,15 +27,4 @@ public class Ticket {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String seatInfo;
-
-	private LocalDateTime createdAt;
-
-	@ManyToOne
-	@JoinColumn(name = "event_id", nullable = false)
-	private Event event;
-
-	@ManyToOne
-	@JoinColumn(name = "purchase_id", nullable = false)
-	private Purchase purchase;
 }
