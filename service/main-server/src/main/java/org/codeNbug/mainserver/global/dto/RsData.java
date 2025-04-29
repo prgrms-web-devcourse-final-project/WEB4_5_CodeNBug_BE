c92600c2aedb6e7529ea32d3c538440515e43d90
HyunmoPark <gusah0p@gmail.com>
2025-04-29 11:28:36 +0900

    feat: RsData 응답 구조 확장 및 페이징 메타데이터 추가

--- service/main-server/src/main/java/org/codeNbug/mainserver/global/dto/RsData.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/global/dto/RsData.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 
 @AllArgsConstructor
 @Getter
@@ -27,8 +28,22 @@ public class RsData<T> {
      */
     private T data;
 
+    /**
+     * 페이지 메타데이터 (페이징 처리된 응답에만 사용)
+     * null일 경우 JSON 응답에서 제외됨
+     */
+    private PageMetadata page;
+
+
     public RsData(String code, String msg) {
-        this(code, msg, null);
+        this(code, msg, null, null);
+    }
+
+    /**
+     * 데이터 포함 생성자 (페이징 없음)
+     */
+    public RsData(String code, String msg, T data) {
+        this(code, msg, data, null);
     }
 
     /**
@@ -41,6 +56,68 @@ public class RsData<T> {
     public int getStatusCode() {
         String statusCodeStr = code.split("-")[0];
         return Integer.parseInt(statusCodeStr);
+    }
+
+
+    /**
+     * 페이징 정보가 포함된 응답 생성
+     * Spring Data의 Page 객체로부터 데이터와 페이징 정보를 추출하여 응답 생성
+     *
+     * @param code 응답 코드 (예: "200-SUCCESS")
+     * @param msg 응답 메시지
+     * @param page 페이징 처리된 데이터
+     * @return 페이징 정보가 포함된 RsData 객체
+     */
+    public static <R> RsData<R> withPage(String code, String msg, Page<R> page) {
+        return new RsData<>(
+                code,
+                msg,
+                (R) page.getContent(),  // 페이지 내용을 데이터로 설정
+                PageMetadata.from(page) // 페이징 메타데이터 생성
+        );
+    }
+
+    /**
+     * 성공 응답 생성 - 페이징 포함 (코드: "200-SUCCESS")
+     *
+     * @param msg 성공 메시지
+     * @param page 페이징 처리된 데이터
+     * @return 성공 응답 객체
+     */
+    public static <R> RsData<R> successWithPage(String msg, Page<R> page) {
+        return withPage("200-SUCCESS", msg, page);
+    }
+
+    /**
+     * 일반적인 성공 응답 생성 (코드: "200-SUCCESS")
+     *
+     * @param msg 성공 메시지
+     * @param data 응답 데이터
+     * @return 성공 응답 객체
+     */
+    public static <R> RsData<R> success(String msg, R data) {
+        return new RsData<>("200-SUCCESS", msg, data);
+    }
+
+    /**
+     * 데이터 없는 성공 응답 생성 (코드: "200-SUCCESS")
+     *
+     * @param msg 성공 메시지
+     * @return 성공 응답 객체
+     */
+    public static <R> RsData<R> success(String msg) {
+        return new RsData<>("200-SUCCESS", msg);
+    }
+
+    /**
+     * 오류 응답 생성
+     *
+     * @param code 오류 코드 (예: "400-BAD_REQUEST")
+     * @param msg 오류 메시지
+     * @return 오류 응답 객체
+     */
+    public static <R> RsData<R> error(String code, String msg) {
+        return new RsData<>(code, msg);
     }
 
 }
