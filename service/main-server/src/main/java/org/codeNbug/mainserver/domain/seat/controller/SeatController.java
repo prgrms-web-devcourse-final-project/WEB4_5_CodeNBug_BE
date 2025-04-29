f457d1408fc4e9d8a831805ae2c7b2c0c31d7eb5
ohnoesganj <sunho3456@naver.com>
2025-04-29 11:07:54 +0900

    Feat: 좌석 취소 컨트롤러 로직 구현

--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/seat/controller/SeatController.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/seat/controller/SeatController.java
@@ -1,10 +1,12 @@
 package org.codeNbug.mainserver.domain.seat.controller;
 
+import org.codeNbug.mainserver.domain.seat.dto.SeatCancelRequest;
 import org.codeNbug.mainserver.domain.seat.dto.SeatSelectRequest;
 import org.codeNbug.mainserver.domain.seat.service.SeatService;
 import org.codeNbug.mainserver.global.dto.RsData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,6 +41,28 @@ public class SeatController {
 			return ResponseEntity.ok(response);
 		} catch (IllegalStateException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body(new RsData("409", "이미 선택된 좌석입니다."));
+		}
+	}
+
+	/**
+	 * 좌석 취소 API
+	 *
+	 * @param eventId           좌석이 포함된 이벤트 ID
+	 * @param seatCancelRequest 사용자가 취소한 좌석 ID 목록
+	 * @return 좌석 취소 결과 응답
+	 */
+	@DeleteMapping("/{event-id}/seats")
+	public ResponseEntity<RsData> CancelSeat(@PathVariable("event-id") Long eventId,
+		@RequestBody SeatCancelRequest seatCancelRequest) {
+		try {
+			seatService.cancelSeat(eventId, seatCancelRequest);
+			RsData response = new RsData(
+				"200",
+				"좌석 취소 성공"
+			);
+			return ResponseEntity.ok(response);
+		} catch (IllegalStateException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new RsData("404", "해당 좌석을 찾을 수 없습니다."));
 		}
 	}
 }
