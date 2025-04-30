954b325a920921f978d6cb4a65ce10cd287ebcab
Park Joung Hyeon <149856960+joungGo@users.noreply.github.com>
2025-04-30 09:32:12 +0900

    Feat: 사용자 인가 (#60)
    * Feat: 인가 기능 구현
    
    * Feat: 테스트 코드 작성(임시-실행 안됨)
    
    * Feat: 사용자 id 요청 메서드
    
    * Feat: 인증된 사용자의 반환 클래스

---
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/mock/TestAuthorizationController.java
@@ -0,0 +1,18 @@
+/*
+package org.codeNbug.mainserver.domain.mock;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/test/auth")
+public class TestAuthorizationController {
+
+    @GetMapping("/public")
+    @ResponseBody
+    public String publicAccess() {
+        return "Public 경로 접근 성공";
+    }
+} */
