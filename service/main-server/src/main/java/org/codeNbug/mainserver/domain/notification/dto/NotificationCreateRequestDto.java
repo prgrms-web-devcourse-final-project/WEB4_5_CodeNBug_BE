12e21d0920b6188ad356aadfc740286b57f4e072
HyunmoPark <gusah0p@gmail.com>
2025-04-30 18:09:25 +0900

    feat: 알림 생성 요청 dto 추가

---
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/notification/dto/NotificationCreateRequestDto.java
@@ -0,0 +1,26 @@
+package org.codeNbug.mainserver.domain.notification.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.codeNbug.mainserver.domain.notification.entity.NotificationEnum;
+
+/**
+ * 알림 생성 요청을 위한 DTO 클래스
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationCreateRequestDto {
+
+    @NotNull(message = "사용자 ID는 필수입니다")
+    private Long userId;
+
+    @NotNull(message = "알림 유형은 필수입니다")
+    private NotificationEnum type;
+
+    @NotEmpty(message = "알림 내용은 필수입니다")
+    private String content;
+}
