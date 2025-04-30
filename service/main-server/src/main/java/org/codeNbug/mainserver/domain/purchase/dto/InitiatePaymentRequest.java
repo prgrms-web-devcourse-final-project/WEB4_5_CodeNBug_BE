d57f6358bf3f96309259dea575d74d059d54cc95
ohnoesganj <sunho3456@naver.com>
2025-04-30 18:38:27 +0900

    Fix: dto 어노테이션 수정

--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/purchase/dto/InitiatePaymentRequest.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/purchase/dto/InitiatePaymentRequest.java
@@ -2,8 +2,10 @@ package org.codeNbug.mainserver.domain.purchase.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class InitiatePaymentRequest {
 	private Long eventId;
--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/purchase/dto/NonSelectTicketPurchaseResponse.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/purchase/dto/NonSelectTicketPurchaseResponse.java
@@ -6,9 +6,12 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NonSelectTicketPurchaseResponse {
 	private Long purchaseId;
 	private Long eventId;
