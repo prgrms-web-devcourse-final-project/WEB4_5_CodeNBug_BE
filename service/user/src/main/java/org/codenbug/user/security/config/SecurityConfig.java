be2d60e37d1735d6883fb68e9ff447685bc93550
ohnoesganj <sunho3456@naver.com>
2025-05-07 12:41:53 +0900

    Bug: webhook 인증 허용

--- service/user/src/main/java/org/codenbug/user/security/config/SecurityConfig.java
+++ service/user/src/main/java/org/codenbug/user/security/config/SecurityConfig.java
@@ -121,6 +121,7 @@ public class SecurityConfig {
 				.requestMatchers("/api/v1/manager/**").permitAll()
 				.requestMatchers("/api/test/auth/public").permitAll()
 				.requestMatchers("/auth/kakao/**").permitAll()
+				.requestMatchers("/webhook/**").permitAll()
 				// Swagger UI 관련 경로 허용
 				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
 				// 나머지 경로는 인증 필요
