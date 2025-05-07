cac97dbb613dc05d49138a39b9935aa4519b08ac
omegafrog <jiwoo22110103@gmail.com>
2025-05-07 14:05:28 +0900

    feat: event entity 통합

--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/event/entity/Event.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/event/entity/Event.java
@@ -2,26 +2,36 @@ package org.codeNbug.mainserver.domain.event.entity;
 
 import java.time.LocalDateTime;
 
-import org.codeNbug.mainserver.domain.manager.entity.EventStatusEnum;
-import org.codeNbug.mainserver.domain.seat.entity.SeatLayout;
-import org.hibernate.validator.constraints.Length;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
+@Entity(name = "ManagerEvent")
 @Table(name = "event")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@FilterDef(name = "activeEventFilter", parameters = @ParamDef(name = "isDeleted", type = Boolean.class))
+@Filter(name = "activeEventFilter", condition = "is_deleted = :isDeleted")
 public class Event {
 
 	@Id
@@ -29,72 +39,34 @@ public class Event {
 	private Long eventId;
 
 	@Column(nullable = false)
-	@Length(min = 5)
-	private String title;
-	@Column(nullable = false)
-	private String thumbnailUrl;
-	@Column(nullable = false, columnDefinition = "TEXT")
-	private String description;
-	@Column(nullable = false)
-	@Min(0)
-	private int ageLimit;
-	@Column(nullable = false, columnDefinition = "TEXT")
-	private String restriction;
-	@Column(nullable = false)
-	@Min(0)
-	private int seatCount;
-	@Column(nullable = false)
-	private LocalDateTime bookingStart;
-	@Column(nullable = false)
-	private LocalDateTime bookingEnd;
-	@Column(nullable = false)
-	private LocalDateTime eventStart;
-	@Column(nullable = false)
-	private LocalDateTime eventEnd;
-	@Embedded
-	private Location location;
-	@Column(nullable = false)
-	private String hallName;
-	@Column(nullable = false)
-	private Long viewCount = 0L;
-	@Column(nullable = false)
-	private EventStatusEnum eventStatus = EventStatusEnum.OPEN;
-	@Column(nullable = false)
-	private boolean seatSelectable = true;
+	@Setter
+	private Long typeId;
 
-	@OneToOne(mappedBy = "event")
-	private SeatLayout seatLayout;
+	@Embedded
+	@Setter
+	private EventInformation information;
+
+	@Setter
+	private LocalDateTime bookingStart;
+
+	@Setter
+	private LocalDateTime bookingEnd;
+	@Setter
+	@Column(columnDefinition = "int default 0")
+	private Integer viewCount;
 
 	@CreatedDate
 	private LocalDateTime createdAt;
+
 	@LastModifiedDate
 	private LocalDateTime modifiedAt;
-
-	public Event() {
-	}
-
-	public Event(String title, String thumbnailUrl, String description, int ageLimit, String restriction, int seatCount,
-		LocalDateTime bookingStart, LocalDateTime bookingEnd, LocalDateTime eventStart, LocalDateTime eventEnd,
-		Location location, String hallName) {
-		if (bookingStart.isAfter(bookingEnd)) {
-			throw new IllegalArgumentException("bookingStart is after bookingEnd");
-		}
-		if (eventStart.isAfter(eventEnd)) {
-			throw new IllegalArgumentException("eventStart is after eventEnd");
-		}
-
-		this.title = title;
-		this.thumbnailUrl = thumbnailUrl;
-		this.description = description;
-		this.ageLimit = ageLimit;
-		this.restriction = restriction;
-		this.seatCount = seatCount;
-		this.bookingStart = bookingStart;
-		this.bookingEnd = bookingEnd;
-		this.eventStart = eventStart;
-		this.eventEnd = eventEnd;
-		this.location = location;
-		this.hallName = hallName;
-	}
-
+	@Setter
+	@Enumerated(EnumType.STRING)
+	private EventStatusEnum status;
+	@Setter
+	@Column(columnDefinition = "boolean default true")
+	private Boolean seatSelectable;
+	@Setter
+	@Column(name = "is_deleted")
+	private Boolean isDeleted = false;
 }
