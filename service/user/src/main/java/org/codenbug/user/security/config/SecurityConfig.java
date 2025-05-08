04705ebfbb45949b537508bdcacf114bcbe4a9d9
omegafrog <jiwoo22110103@gmail.com>
2025-05-08 12:20:22 +0900

    fix: cors 설정 변경

--- service/user/src/main/java/org/codenbug/user/security/config/SecurityConfig.java
+++ service/user/src/main/java/org/codenbug/user/security/config/SecurityConfig.java
@@ -127,8 +127,8 @@ public class SecurityConfig {
 				.anyRequest().authenticated())
 			.exceptionHandling(exceptionHandling -> exceptionHandling
 				.authenticationEntryPoint((request, response, authException) -> {
-					authException.printStackTrace();
-					log.info(request.getCookies().toString() + "\n" + request.getHeaderNames().toString() + "\n");
+					// authException.printStackTrace();
+					// log.info(request.getCookies().toString() + "\n" + request.getHeaderNames().toString() + "\n");
 					response.setStatus(HttpStatus.UNAUTHORIZED.value());
 					response.setContentType("application/json");
 					response.getWriter().write("{\"code\":\"401-UNAUTHORIZED\",\"msg\":\"인증 정보가 필요합니다.\"}");
