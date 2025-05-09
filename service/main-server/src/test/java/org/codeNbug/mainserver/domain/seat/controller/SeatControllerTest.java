fac91d4fbbce03403435203ced7eb21362261de9
ohnoesganj <sunho3456@naver.com>
2025-05-09 15:32:20 +0900

    test: 좌석 선택 테스트 코드에 Redis 적용

--- service/main-server/src/test/java/org/codeNbug/mainserver/domain/seat/controller/SeatControllerTest.java
+++ service/main-server/src/test/java/org/codeNbug/mainserver/domain/seat/controller/SeatControllerTest.java
@@ -1,16 +1,19 @@
 package org.codeNbug.mainserver.domain.seat.controller;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.codeNbug.mainserver.domain.event.entity.Event;
 import org.codeNbug.mainserver.domain.event.entity.EventInformation;
 import org.codeNbug.mainserver.domain.event.entity.EventStatusEnum;
 import org.codeNbug.mainserver.domain.manager.repository.EventRepository;
+import org.codeNbug.mainserver.domain.seat.dto.SeatSelectRequest;
 import org.codeNbug.mainserver.domain.seat.entity.Seat;
 import org.codeNbug.mainserver.domain.seat.entity.SeatGrade;
 import org.codeNbug.mainserver.domain.seat.entity.SeatGradeEnum;
@@ -18,27 +21,38 @@ import org.codeNbug.mainserver.domain.seat.entity.SeatLayout;
 import org.codeNbug.mainserver.domain.seat.repository.SeatGradeRepository;
 import org.codeNbug.mainserver.domain.seat.repository.SeatLayoutRepository;
 import org.codeNbug.mainserver.domain.seat.repository.SeatRepository;
+import org.codeNbug.mainserver.domain.seat.service.RedisLockService;
 import org.codenbug.user.domain.user.entity.User;
 import org.codenbug.user.domain.user.repository.UserRepository;
 import org.codenbug.user.redis.service.TokenService;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SeatControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
@@ -62,20 +76,31 @@ class SeatControllerTest {
 	private SeatGradeRepository seatGradeRepository;
 
 	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private RedisLockService redisLockService;
 
 	@Autowired
 	private TokenService tokenService;
 
-	private String testToken;
-	private User testUser;
-	private Event testEvent;
+	@Autowired
+	UserDetailsService userDetailsService;
 
-	@BeforeEach
-	void setUp() throws JSONException {
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private StringRedisTemplate redisTemplate;
+
+	public static final String ENTRY_TOKEN_STORAGE_KEY_NAME = "ENTRY_TOKEN";
+
+	private static String testToken;
+	private static User testUser;
+	private static Event testEvent;
+
+	@BeforeAll
+	public void setUp() throws JSONException {
 		// 테스트용 사용자 생성
 		testUser = User.builder()
-			.email("test@example.com")
+			.email("test341qqweew@example.com")
 			.password(passwordEncoder.encode("Test1234!"))
 			.name("테스트")
 			.age(25)
@@ -88,6 +113,11 @@ class SeatControllerTest {
 
 		// 테스트용 토큰 생성
 		TokenService.TokenInfo tokenInfo = tokenService.generateTokens(testUser.getEmail());
+		UserDetails userDetails = userDetailsService.loadUserByUsername(testUser.getEmail());
+		UsernamePasswordAuthenticationToken authentication =
+			new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
 		testToken = tokenInfo.getAccessToken();
 
 		// 테스트용 행사 생성
@@ -177,7 +207,7 @@ class SeatControllerTest {
 				SeatGradeEnum gradeEnum = SeatGradeEnum.fromString(grade);
 				SeatGrade seatGrade = gradeMap.get(gradeEnum);
 
-				Seat seat = Seat.builder()
+				Seat testSeat = Seat.builder()
 					.location(seatName)
 					.grade(seatGrade)
 					.layout(seatLayout)
@@ -185,42 +215,73 @@ class SeatControllerTest {
 					.available(true)
 					.build();
 
-				seatRepository.save(seat);
+				seatRepository.save(testSeat);
 			}
 		}
 	}
 
+	@BeforeEach
+	void setUpRedis() {
+		// 테스트용 entry token 저장
+		redisTemplate.opsForHash().put(
+			ENTRY_TOKEN_STORAGE_KEY_NAME,
+			String.valueOf(testUser.getUserId()),
+			"\"" + testToken + "\""
+		);
+	}
+
+	// @AfterAll
+	// void tearDown() {
+	// 	userRepository.deleteAll();
+	// 	seatLayoutRepository.deleteAll();
+	// 	seatRepository.deleteAll();
+	// 	seatGradeRepository.deleteAll();
+	// 	eventRepository.deleteAll();
+	// 	redisTemplate.opsForHash().delete(
+	// 		ENTRY_TOKEN_STORAGE_KEY_NAME,
+	// 		String.valueOf(testUser.getUserId())
+	// 	);
+	// }
+
 	@Test
+	@Commit
 	@DisplayName("좌석 조회 성공")
 	void testGetSeatLayout() throws Exception {
-		mockMvc.perform(get("/api/v1/event/{eventId}/seats", 1)
+		mockMvc.perform(get("/api/v1/event/{eventId}/seats", testEvent.getEventId())
 				.header("Authorization", "Bearer " + testToken))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.code").value("200"))
 			.andExpect(jsonPath("$.msg").value("좌석 조회 성공"))
 			.andExpect(jsonPath("$.data.seats[0].location").value("A1"))
 			.andExpect(jsonPath("$.data.seats[0].grade").value("VIP"))
-			.andExpect(jsonPath("$.data.seats[0].available").value(true))
 			.andExpect(jsonPath("$.data.seats[1].location").value("A2"))
-			.andExpect(jsonPath("$.data.seats[1].grade").value("R"))
-			.andExpect(jsonPath("$.data.seats[1].available").value(true));
+			.andExpect(jsonPath("$.data.seats[1].grade").value("R"));
 	}
 
-	// @Test
-	// @DisplayName("좌석 선택 성공")
-	// void testSelectSeat() throws Exception {
-	// 	SeatSelectRequest request = new SeatSelectRequest();
-	// 	request.setSeatIds(List.of(101L, 102L)); // 실제 좌석 ID에 맞게 조정
-	//
-	// 	mockMvc.perform(post("/api/v1/event/{eventId}/seats", 1)
-	// 			.header("entryAuthToken", "entry_token_example") // 실제 검증 로직 통과할 값으로
-	// 			.contentType(MediaType.APPLICATION_JSON)
-	// 			.content(objectMapper.writeValueAsString(request))
-	// 			.header("Authorization", "Bearer " + testToken))
-	// 		.andExpect(status().isOk())
-	// 		.andExpect(jsonPath("$.code").value("200"))
-	// 		.andExpect(jsonPath("$.message").value("좌석 선택 성공"));
-	// }
+	@Test
+	@DisplayName("지정석 좌석 선택 - Redis 락 획득 확인")
+	void selectSeat_withRedisLock() throws Exception {
+		List<Seat> availableSeats = seatRepository.findFirstByEventIdAndAvailableTrue(testEvent.getEventId());
+		Seat seat = availableSeats.get(0);
+
+		SeatSelectRequest request = new SeatSelectRequest();
+		request.setSeatList(List.of(seat.getId()));
+		request.setTicketCount(1);
+
+		String json = objectMapper.writeValueAsString(request);
+
+		mockMvc.perform(post("/api/v1/event/{eventId}/seats", testEvent.getEventId())
+				.header("Authorization", "Bearer " + testToken)
+				.header("entryAuthToken", testToken)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+			.andExpect(status().isOk());
+
+		String redisKey =
+			"seat:lock:" + testUser.getUserId() + ":" + testEvent.getEventId() + ":" + request.getSeatList().getFirst();
+		String lockValue = redisLockService.getLockValue(redisKey);
+		assertThat(lockValue).isNotNull();
+	}
 	//
 	// @Test
 	// @DisplayName("좌석 취소 성공")
