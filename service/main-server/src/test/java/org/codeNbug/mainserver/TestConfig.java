4f14e4738b01bdea09d803ad018812430e4a6acc
omegafrog <jiwoo22110103@gmail.com>
2025-05-12 15:25:25 +0900

    feat: .

--- service/main-server/src/test/java/org/codeNbug/mainserver/TestConfig.java
+++ service/main-server/src/test/java/org/codeNbug/mainserver/TestConfig.java
@@ -2,6 +2,8 @@ package org.codeNbug.mainserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -14,5 +16,22 @@ public class TestConfig {
 		return new ObjectMapper()
 			.registerModule(new JavaTimeModule())
 			.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	}
+
+	static {
+		GenericContainer redisContainer = new GenericContainer(DockerImageName.parse("redis:alpine"))
+			.withExposedPorts(6379)
+			.withReuse(true);
+
+		redisContainer.start();
+		System.setProperty("spring.data.redis.host", redisContainer.getHost());
+		System.setProperty("spring.data.redis.port", redisContainer.getMappedPort(6379).toString());
+
+		GenericContainer mysqlContainer = new GenericContainer(DockerImageName.parse("mysql/mysql-server:8.0"))
+			.withExposedPorts(3306)
+			.withReuse(true);
+		mysqlContainer.start();
+		System.setProperty("spring.datasource.url",
+			"jdbc:mysql://" + mysqlContainer.getHost() + ":" + mysqlContainer.getMappedPort(3306) + "/ticketonTest");
 	}
 }
