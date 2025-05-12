73c4d9d12e087a134c82b2506ace00ea2c16fdd3
장선호 <92978663+ohnoesganj@users.noreply.github.com>
2025-05-12 09:41:18 +0900

    Test/좌석 선택 취소 테스트 (#194)
    * refactor: userControllerTest 로그아웃 실패 메시지 오류 수정
    
    * test: 좌석 선택 테스트 코드 작성
    
    * test: 좌석 선택 테스트 코드에 Redis 적용
    
    * test: 좌석 취소 테스트 코드 작성 (Redis 적용)
    
    * test: 테스트 후 데이터 모두 삭제하는 로직 추가

--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/seat/entity/SeatGrade.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/seat/entity/SeatGrade.java
@@ -15,6 +15,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,6 +26,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Builder
 public class SeatGrade {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
