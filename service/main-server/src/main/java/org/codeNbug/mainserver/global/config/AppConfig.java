93b38a758d89fd001e4eb058448d20f80b8b95f1
omegafrog <jiwoo22110103@gmail.com>
2025-05-17 10:47:55 +0900

    Feat: 페이징 지원 및 설정 추가
    EventController에서 이벤트 목록 조회 시 Page를 사용하도록 수정하여 페이징 기능을 지원합니다. 또한, AppConfig에 @EnableSpringDataWebSupport를 추가하여 Spring Data Web을 통한 페이지 직렬화를 활성화했습니다.

--- service/main-server/src/main/java/org/codeNbug/mainserver/global/config/AppConfig.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/global/config/AppConfig.java
@@ -3,6 +3,7 @@ package org.codeNbug.mainserver.global.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
@@ -10,6 +11,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @ComponentScan(basePackages = {"org.codeNbug.mainserver", "org.codenbug.user",
 	"org.codenbug.common"})
+@EnableSpringDataWebSupport(
+	pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 public class AppConfig {
 
 }
