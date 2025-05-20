518f1a8c4661c9ede0807c714bdd90d9d085abc9
HyunmoPark <gusah0p@gmail.com>
2025-05-20 18:07:03 +0900

    fix: 코드테스트 충돌 해결

--- service/main-server/src/test/java/org/codeNbug/mainserver/domain/notification/controller/NotificationControllerTest.java
+++ service/main-server/src/test/java/org/codeNbug/mainserver/domain/notification/controller/NotificationControllerTest.java
@@ -18,7 +18,6 @@ import org.codeNbug.mainserver.domain.notification.service.NotificationService;
 import org.codeNbug.mainserver.global.util.SecurityUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -224,28 +223,26 @@ class NotificationControllerTest {
 		verify(notificationService, times(1)).deleteNotification(eq(notificationId), anyLong());
 	}
 
-    @Test
-    @WithMockUser
-    @DisplayName("다건 알림 삭제 테스트")
-    @Disabled
-    void deleteNotifications() throws Exception {
-        // given
-        List<Long> notificationIds = Arrays.asList(1L, 2L);
-        NotificationDeleteRequestDto request = new NotificationDeleteRequestDto(notificationIds);
+	 @Test
+	 @WithMockUser
+	 @DisplayName("다건 알림 삭제 테스트")
+	 void batchDeleteNotifications() throws Exception {
+		 // given
+		 List<Long> notificationIds = Arrays.asList(1L, 2L);
+		 NotificationDeleteRequestDto request = new NotificationDeleteRequestDto(notificationIds);
 
-        doNothing().when(notificationService).deleteNotifications(eq(notificationIds), anyLong());
+		 doNothing().when(notificationService).deleteNotifications(eq(notificationIds), anyLong());
 
-        // when & then
-        mockMvc.perform(delete("/api/v1/notifications")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value("200-SUCCESS"))
-                .andExpect(jsonPath("$.msg").value("알림 삭제 성공"));
+		 // when & then
+		 mockMvc.perform(post("/api/v1/notifications/batch-delete")
+						 .contentType(MediaType.APPLICATION_JSON)
+						 .content(objectMapper.writeValueAsString(request)))
+				 .andExpect(status().isOk())
+				 .andExpect(jsonPath("$.code").value("200-SUCCESS"))
+				 .andExpect(jsonPath("$.msg").value("알림 삭제 성공"));
 
-        verify(notificationService, times(1)).deleteNotifications(eq(notificationIds), anyLong());
-    }
-
+		 verify(notificationService, times(1)).deleteNotifications(eq(notificationIds), anyLong());
+	 }
 	@Test
 	@WithMockUser
 	@DisplayName("모든 알림 삭제 테스트")
