c8c3d70fde6fe60a0c30fea1d579c156153d1484
HyunmoPark <gusah0p@gmail.com>
2025-05-26 11:32:05 +0900

    fix: User수정에 age, sex 추가

--- service/main-server/src/main/java/org/codeNbug/mainserver/domain/user/dto/request/UserUpdateRequest.java
+++ service/main-server/src/main/java/org/codeNbug/mainserver/domain/user/dto/request/UserUpdateRequest.java
@@ -1,5 +1,6 @@
 package org.codeNbug.mainserver.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -20,6 +21,12 @@ public class UserUpdateRequest {
     @Size(min = 2, max = 50, message = "이름은 2자 이상 50자 이하여야 합니다.")
     @NotNull
     private String name;
+
+    @NotNull(message = "나이는 필수 입력 항목입니다.")
+    private Integer age;
+
+    @NotBlank(message = "성별은 필수 입력 항목입니다.")
+    private String sex;
 
     @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다. (예: 010-1234-5678)")
     @NotNull
